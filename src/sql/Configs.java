a5c2bf426bd1788bc17056b840426248b3b2df14
Вадим <vadim.malynich@mail.ru>
2020-12-13 19:33:13 +0300

    server

--- src/sql/Configs.java
+++ src/sql/Configs.java
@@ -4,6 +4,6 @@ public class Configs {
     protected String dbHost = "localhost";
     protected String dbPort = "3306";
     protected String dbUser = "root";
-    protected String dbPass = "1v4a5d2i9m5";
+    protected String dbPass = "";
     protected String dbName = "cursach";
 }
